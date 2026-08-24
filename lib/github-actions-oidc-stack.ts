3c8bb485281a92c8db0dbae971d3567d7182e87c
Evan Waite <evanwaite2024@u.northwestern.edu>
2026-08-23 22:41:45 -0700

    Add GitHub OIDC deploy roles

--- lib/github-actions-oidc-stack.ts
+++ lib/github-actions-oidc-stack.ts
@@ -3,61 +3,71 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class GitHubActionsOidcStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    const repo = 'Evan63w/GetItDone';
+        const repo = 'Evan63w/GetItDone';
 
-    const githubOidcProvider = new iam.OpenIdConnectProvider(this, 'GitHubActionsOidc', {
-      url: 'https://token.actions.githubusercontent.com',
-      clientIds: ['sts.amazonaws.com'],
-    });
+        const githubOidcProvider = new iam.OpenIdConnectProvider(this, 'GitHubActionsOidc', {
+            url: 'https://token.actions.githubusercontent.com',
+            clientIds: ['sts.amazonaws.com'],
+        });
 
-    const allowCdkDeploy = new iam.PolicyStatement({
-      sid: 'AllowCdkDeploy',
-      effect: iam.Effect.ALLOW,
-      actions: ['*'],
-      resources: ['*'],
-    });
+        const allowCdkDeploy = new iam.PolicyStatement({
+            sid: 'AllowCdkDeploy',
+            effect: iam.Effect.ALLOW,
+            actions: ['*'],
+            resources: ['*'],
+        });
 
-    const betaRole = new iam.Role(this, 'GitHubActionsBetaRole', {
-      roleName: 'GetItDoneGitHubActionsDeployRoleBeta',
-      assumedBy: new iam.FederatedPrincipal(
-        githubOidcProvider.openIdConnectProviderArn,
-        {
-          StringEquals: {
-            'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com',
-            'token.actions.githubusercontent.com:sub': `repo:${repo}:environment:beta`,
-          },
-        },
-        'sts:AssumeRoleWithWebIdentity'
-      ),
-    });
+        const betaRole = new iam.Role(this, 'GitHubActionsBetaRole', {
+            roleName: 'GetItDoneGitHubActionsDeployRoleBeta',
+            assumedBy: new iam.FederatedPrincipal(
+                githubOidcProvider.openIdConnectProviderArn,
+                {
+                    StringEquals: {
+                        'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com',
+                    },
+                    StringLike: {
+                        'token.actions.githubusercontent.com:sub': [
+                            `repo:${repo}:environment:beta`,
+                            `repo:${repo}:ref:refs/heads/main`,
+                        ],
+                    },
+                },
+                'sts:AssumeRoleWithWebIdentity'
+            ),
+        });
 
-    betaRole.addToPolicy(allowCdkDeploy);
+        betaRole.addToPolicy(allowCdkDeploy);
 
-    const prodRole = new iam.Role(this, 'GitHubActionsProdRole', {
-      roleName: 'GetItDoneGitHubActionsDeployRoleProd',
-      assumedBy: new iam.FederatedPrincipal(
-        githubOidcProvider.openIdConnectProviderArn,
-        {
-          StringEquals: {
-            'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com',
-            'token.actions.githubusercontent.com:sub': `repo:${repo}:environment:prod`,
-          },
-        },
-        'sts:AssumeRoleWithWebIdentity'
-      ),
-    });
+        const prodRole = new iam.Role(this, 'GitHubActionsProdRole', {
+            roleName: 'GetItDoneGitHubActionsDeployRoleProd',
+            assumedBy: new iam.FederatedPrincipal(
+                githubOidcProvider.openIdConnectProviderArn,
+                {
+                    StringEquals: {
+                        'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com',
+                    },
+                    StringLike: {
+                        'token.actions.githubusercontent.com:sub': [
+                            `repo:${repo}:environment:prod`,
+                            `repo:${repo}:ref:refs/heads/main`,
+                        ],
+                    },
+                },
+                'sts:AssumeRoleWithWebIdentity'
+            ),
+        });
 
-    prodRole.addToPolicy(allowCdkDeploy);
+        prodRole.addToPolicy(allowCdkDeploy);
 
-    new cdk.CfnOutput(this, 'BetaRoleArn', {
-      value: betaRole.roleArn,
-    });
+        new cdk.CfnOutput(this, 'BetaRoleArn', {
+            value: betaRole.roleArn,
+        });
 
-    new cdk.CfnOutput(this, 'ProdRoleArn', {
-      value: prodRole.roleArn,
-    });
-  }
+        new cdk.CfnOutput(this, 'ProdRoleArn', {
+            value: prodRole.roleArn,
+        });
+    }
 }
